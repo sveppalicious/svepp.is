beeba1175cbff2e2131fe22f66c96ca6b5754e7f
sveppalicious <sverrir.sigurdarson@gmail.com>
2017-03-10 21:00:39 +0000

    add react

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {deepOrange500} from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
